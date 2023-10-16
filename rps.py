dea49e4f1626f66dbc89e33403b3d132aa687805
yensama7 <147042568+yensama7@users.noreply.github.com>
2023-10-16 10:27:11 +0100

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,67 +1,71 @@
 #Play a game of Rock, Paper and scissors with Python
-import random
-game_count = 0
-your_wins = 0
-your_loss = 0
+def game():
+    import random
+    game_count = 0
+    your_wins = 0
+    your_loss = 0
 
-while True:
-    print("")
-    print("....................")
-    print("Enter done to end game")
-    print("....................")
-    inp = input(
-    "Enter ... \n1 for Rock, \n2 for Paper, \n3 for Scissors \n"
-)
-    if inp.lower() == "done" :
+    while True:
+        print("")
         print("....................")
-        print("Game count: ", game_count , "\n Your wins: ", your_wins, "\n your loss: ", your_loss)
+        print("Enter done to end game")
         print("....................")
-        break
-    try:
-        player = int(inp)
-    except:
-        print("Enter 1, 2 or 3 😒")
-        continue
-        quit()
+        inp = input(
+        "Enter ... \n1 for Rock, \n2 for Paper, \n3 for Scissors \n"
+         )
+        if inp.lower() == "done" :
+            print("....................")
+            print("Game count: ", game_count , "\n Your wins: ", your_wins, "\n your loss: ", your_loss)
+            print("....................")
+            break
+        try:
+            player = int(inp)
+        except:
+            print("Enter 1, 2 or 3 😒")
+            continue
+            quit()
 
-    if player < 1 or player > 3 :
-        print("enter 1, 2 or 3😒")
-        continue
+        if player < 1 or player > 3 :
+            print("enter 1, 2 or 3😒")
+            continue
 
-    compchoice = random.choice('123')
-    comp= int(compchoice)
+        compchoice = random.choice('123')
+        comp= int(compchoice)
 
-    def rps(inp):
-        if inp == 1:
-            return "Rock"
-        elif inp == 2:
-            return "Paper"
-        elif inp == 3:
-            return "Scissors"
+        def rps(inp):
+            if inp == 1:
+                return "Rock"
+            elif inp == 2:
+                return "Paper"
+            elif inp == 3:
+                return "Scissors"
 
-    print("you chose ", rps(player) )
-    print("python  chose ", rps(comp) )
-
-
-    if player == 1 and comp == 3:
-        your_wins += 1
-        print("you win 🎉🥳")
-
-
-    elif player == 2 and comp == 1:
-        your_wins += 1
-        print("you win 🎉🥳")
+        print("you chose ", rps(player) )
+        print("python  chose ", rps(comp) )
 
 
-    elif player == 3 and comp == 2:
-        your_wins += 1
-        print("you win 🎉🥳")
+        if player == 1 and comp == 3:
+            your_wins += 1
+            print("you win 🎉🥳")
 
-    elif player == comp :
-        print("Tie Game😮")
 
-    else:
-        your_loss += 1
-        print("Python wins 🐍")
+        elif player == 2 and comp == 1:
+            your_wins += 1
+            print("you win 🎉🥳")
 
-    game_count += 1
+
+        elif player == 3 and comp == 2:
+            your_wins += 1
+            print("you win 🎉🥳")
+
+        elif player == comp :
+            print("Tie Game😮")
+
+        else:
+            your_loss += 1
+            print("Python wins 🐍")
+
+        game_count += 1
+
+play = game()
+play()
